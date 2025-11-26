c0cf3ed8057f68071a0f534996c8eb636a0a217d
shresthakhya <shresthakhya7@gmail.com>
2025-11-26 16:50:36 +0545

    Remove .next from tracking

--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_284caf23._.js",
-  "static/chunks/components_113ea867._.js"
-],
-    source: "dynamic"
-});
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MsrkuX9pYzhObewY3MOkIhd3BpHCWnvJqAPuuK1KsNg=\"\n}"
